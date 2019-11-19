15b5ae54552885f47c8e634002e5d37a5aade13b
Selam Gessese <sygessese@gmail.com>
2019-11-18 21:06:54 -0800

    fetching location, connected to server

---
+++ App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import samplequery from './samplequery';
+import Tabs from './src/Tabs';
+import Geolocation from '@react-native-community/geolocation';
+import axios from 'axios';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      home: true,
+      locationSet: false,
+      lat: '',
+      long: '',
+      locError: '',
+      foods: '',
+      foodsError: '',
+    };
+    this.updateHome = this.updateHome.bind(this);
+    this.getLoc = this.getLoc.bind(this);
+  }
+
+  getFoods(lat, long) {
+    axios.get('http://localhost:3000/', { lat, long })
+      .then(foods => this.setState({ foods: foods.data }))
+      .catch(foodsError => this.setState({ foodsError }))
+  }
+
+  getLoc() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        var lat = parseFloat(position.coords.latitude);
+        var long = parseFloat(position.coords.longitude);
+        this.setState({ lat, long, locationSet: true }, () => this.getFoods(this.state.lat, this.state.long))
+      },
+      (locError) => this.setState({ locError: locError.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
+  updateHome(bool) {
+    if (bool !== this.state.home) {
+      this.setState({ home: bool })
+    }
+  }
+
+  // componentDidMount() {
+  //   // this.getLoc();
+  // }
+
+  render() {
+    const search = <View style={styles.searchContainer}>
+      <Text style={styles.searchTitle} onPress={this.getLoc}>Hungry</Text></View>;
+
+    const found = <View style={styles.foundContainer}>
+      <Text style={styles.foundTitle}>Hungry</Text>
+      <Text style={styles.foundDescription}>
+        lat: {this.state.lat} long: {this.state.long}
+        foods: {this.state.foods}
+      </Text></View>;
+
+    return (
+      <>
+        {this.state.locationSet ? found : search}
+        <Tabs updateHome={this.updateHome} />
+      </>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  foundContainer: {
+    marginTop: 52,
+    paddingHorizontal: 24,
+  },
+  foundTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+    textAlign: 'center',
+  },
+  foundDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  searchTitle: {
+    fontSize: 50,
+    fontWeight: '600',
+    color: Colors.black,
+    textAlign: 'center',
+  },
+  searchContainer: {
+    marginTop: 300,
+    fontSize: 18,
+    fontWeight: '600',
+    color: Colors.dark,
+  },
+  tabs: {
+    marginBottom: 0,
+    backgroundColor: Colors.yellow,
+  },
+});
+
+export default App;
