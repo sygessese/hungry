a759a4fb7feb8b6253fc868b69e45dfb34373a9e
Selam Gessese <sygessese@gmail.com>
2019-11-19 02:58:04 -0800

    flips between search and saved screens

--- src/Tabs.js
+++ src/Tabs.js
@@ -3,24 +3,49 @@ import React, { Component } from 'react';
 import {
   View,
   StyleSheet,
-  Button,
+  TouchableHighlight,
+  Text,
 } from 'react-native';
 
 
 const styles = StyleSheet.create({
   fixToText: {
+    display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: 100,
+    justifyContent: 'space-around',
+    width: '100%',
+    height: '10%',
+    fontSize: 50,
   },
+  buttonSelected: {
+    backgroundColor: 'lightgrey',
+    width: '50%',
+    height: '100%',
+  },
+  button: {
+    backgroundColor: 'beige',
+    width: '50%',
+    height: '100%',
+  },
+  script: {
+    fontSize: 15,
+    alignSelf: 'center',
+    marginTop: '15%',
+  }
 });
 
 export default class Tabs extends Component {
   render() {
     return (
       <View style={styles.fixToText}>
-        <Button title="Find" onPress={() => this.props.updateHome(true)} />
-        <Button title="Faves" onPress={() => this.props.updateHome(false)} />
+        <TouchableHighlight underlayColor="lightgrey" onPress={() => this.props.updateHome(true)} style={this.props.home ? styles.buttonSelected : styles.button}>
+          <Text style={styles.script}>Find</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight underlayColor="lightgrey" onPress={() => this.props.updateHome(false)} style={this.props.home ? styles.button : styles.buttonSelected}>
+          <Text style={styles.script}>GOAT</Text>
+        </TouchableHighlight>
+
       </View>
     );
   }
