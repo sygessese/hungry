962bfa1fb67039f81bea15065e68d8c32f71c275
Selam Gessese <sygessese@gmail.com>
2019-11-20 09:55:32 -0800

    making graphql queries to API

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const app = express();
 const port = 3000;
 // const yelpDev = require('./yelpAPI');
 const db = require('./db/connection');
+const request = require('request');
+
+var headers = {
+  'Authorization': 'Bearer Cltrz7Tt9bodyBWjZV7wzKKIQbpG_yg4XbHP-0KOiIzvP0eRKK-jNKyhwajRAAInJIV_7-NBFmUlykKJX-8worvIvpf40h5f9Jstlx-zkwOF4a3F2RvqpixbolnQXXYx',
+  'Content-Type': 'application/graphql'
+};
+
+var dataString = '{ search( term:"food", latitude: 37.785834, longitude: -122.406417, open_now: true, limit: 5 ) { business { name rating review_count photos id categories { title } price } }}';
+
+var options = {
+  url: 'https://api.yelp.com/v3/graphql',
+  method: 'POST',
+  headers: headers,
+  body: dataString
+};
 
 app.post('/api/newrestaurant/', (req, res) => {
   const info = req.query;
@@ -12,11 +27,20 @@ app.post('/api/newrestaurant/', (req, res) => {
 });
 
 app.get('/api/restaurants/', (req, res) => {
-  res.send(['Restaurant one', 'Restaurant two', 'Restaurant three', 'Restaurant four', 'Restaurant five', 'Restaurant six', 'Restaurant seven', 'Restaurant eight', 'Restaurant nine', 'Restaurant ten'])
+  res.send(['Canlis', `Walrus and Carpenter`, 'El Borracho', 'Japonessa', 'Il Corvo', 'Le Pichet', 'Salumi', 'Macrina', 'Rocco', 'Mediterranean Mix', 'Poke Bar'])
   // db.Read()
   //   .then(restaurants => res.send(restaurants))
   //   .catch(error => res.send(error))
 });
+
+app.get('/test', (req, res) => {
+  request(options, (error, response, body) => {
+    if (error) {
+      res.sendStatus(500)
+    }
+    res.send(body)
+  })
+})
 
 app.put('/api/restaurants/', (req, res) => {
   const id = req.query.id;
